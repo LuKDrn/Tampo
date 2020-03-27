5fd6d408f6b0ed06a4790d82af609e7625eefb50
dilha <dilha@DESKTOP-D5NKTFA>
2020-03-27 11:51:43 +0100

    debut message

--- Components/Message.js
+++ Components/Message.js
@@ -1,21 +1,48 @@
 import React from 'react'
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native'
+import {View, Text, StyleSheet, SafeAreaView, Platform} from 'react-native';
+import { GiftedChat } from "react-native-gifted-chat";
+import Fire from "../Fire";
 
 export default class Message extends React.Component {
+    state = {
+        messages: []
+    }
+
+    get user() {
+        return {
+            _id: Fire.uid,
+            name: this.props.navigation.state.params.name
+        }
+    }
+
+    ComponentDidMount() {
+        Fire.get(messages => 
+            this.setState(previous => ({
+                messages: GiftedChat.append(previous.messages, message)
+            }))
+        );
+    }
+
+    ComponentWillUnmount() {
+        Fire.off();
+    }
+
     render () {
+        const chat = <GiftedChat message={this.state.messages} onSend={Fire.Send} user={this.user} />
+
+        if (Platform.OS === 'android') {
+            return (
+                <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding" keyboradVerticalOffset={30} enabled>
+                    {chat}
+                </KeyboardAvoidingView>
+            );
+        }
         return (
-            <SafeAreaView style={styles.container}>
-                <Text>Messages</Text>
-            </SafeAreaView>
+            <View style={styles.container}>
+                <Text>Sreen</Text>
+            </View>
         )
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#14142d'
-    }
-})
+
